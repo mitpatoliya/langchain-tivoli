8664777e1bf3f104619849bc028f9d3de875cbda
mittal <mitpatoliya@gmail.com>
2025-12-17 13:07:42 +0530

    added tivoli search tool

--- main.py
+++ main.py
@@ -6,22 +6,26 @@ from langchain_core.prompts import PromptTemplate
 from langchain.tools import tool
 from langchain.agents import create_agent
 from tavily import TavilyClient
+from langchain_tavily import TavilySearch
 load_dotenv()
-tavily= TavilyClient()
 
-@tool
-def get_current_weather(city:str) -> str:
-    """Tool that gets the current weather for a given city
-    
-    Args:
-        city: The city to get the weather for
-    Returns:
-        The current weather for the city
-    """
-    print(f"Getting the weather for {city}")
-    return tavily.search(query=f"weather in {city}")
+#tavily= TavilyClient()
 
-tools= [get_current_weather]
+# @tool
+# def get_current_weather(city:str) -> str:
+#     """Tool that gets the current weather for a given city
+#     
+#     Args:
+#         city: The city to get the weather for
+#     Returns:
+#         The current weather for the city
+#     """
+#     print(f"Getting the weather for {city}")
+#     return tavily.search(query=f"weather in {city}")
+
+
+
+tools= [TavilySearch()]
 model = ChatOpenAI(model_name="gpt-4o-mini", temperature=0.0)
 #model = ChatOllama(model="gemma3:1b", temperature=0.0)
 agent=create_agent(model, tools)
